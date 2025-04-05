6c968823b3357dc085364ffc2c5e6679c4dc45b8
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-04-05 08:48:09 +0800

    ClawControl

---
+++ app/Http/Controllers/CrabController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Crab;
+use App\Http\Requests\StoreCrabRequest;
+use App\Http\Requests\UpdateCrabRequest;
+
+class CrabController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCrabRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Crab $crab)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Crab $crab)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCrabRequest $request, Crab $crab)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Crab $crab)
+    {
+        //
+    }
+}
